5edb6e0d9cb1550043d77d283728f87040d8b9f2
kodziarz <58184304+kodziarz@users.noreply.github.com>
2022-05-27 09:04:46 +0200

    Add files via upload

---
+++ main/java/com/example/test_lenar_01/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.test_lenar_01;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private LinearLayout accountLinLayout;
+    private LinearLayout searchLinLayout;
+    private LinearLayout createLinLayout;
+    private LinearLayout rollLinLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        accountLinLayout = findViewById(R.id.accountLinLayout);
+        searchLinLayout = findViewById(R.id.searchLinLayout);
+        createLinLayout = findViewById(R.id.createLinLayout);
+        rollLinLayout = findViewById(R.id.rollLinLayout);
+
+
+        accountLinLayout.setOnClickListener((v) -> {
+            Intent intent = new Intent(MainActivity.this, AccountActivity.class);
+            startActivity(intent);
+        });
+
+        searchLinLayout.setOnClickListener((v) -> {
+            Intent intent = new Intent(MainActivity.this, SearchActivity.class);
+            startActivity(intent);
+        });
+
+        createLinLayout.setOnClickListener((v) -> {
+            Intent intent = new Intent(MainActivity.this, CreateActivity.class);
+            startActivity(intent);
+        });
+
+        rollLinLayout.setOnClickListener((v) -> {
+            Intent intent = new Intent(MainActivity.this, RollActivity.class);
+            startActivity(intent);
+        });
+    }
+}
